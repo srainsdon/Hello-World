7a8837c704909f5bc0d5b8e4f1714c682b8d63ea
Seth Rainsdon <srainsdon>
2015-05-28 00:41:55 -0700

    recursive function
    I made a working recursive function

--- main.cpp
+++ main.cpp
@@ -3,16 +3,27 @@
 
 using namespace std;
 
+bool DeBug = true;
+
 int sendMsg(string msg) {
     cout << msg << endl;
     return 0;
+}
+
+int TestNow(int x) {
+    if(DeBug){cout << x << endl;}
+    if(x==1) {
+    return 1;
+    }else{
+        return x+TestNow(x-1);
+    }
 }
 
 int main()
 {
     string hr = "------------------------------";
     cout << "/*" << hr << endl;
-    cout << "This is a test." << endl;
+    cout << "This is a test. " << TestNow(50) << endl;
     cout << hr << "*/" << endl;
     return 0;
 }
