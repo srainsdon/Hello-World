65fb75e619f96ce14d40e392ef6ae0c36467601d
Seth Rainsdon <srainsdon>
2015-05-28 15:02:00 -0700

    Working our Server

--- main.cpp
+++ main.cpp
@@ -6,25 +6,15 @@ using namespace std;
 bool DeBug = false;
 
 int sendMsg(string msg) {
+    string hr = std::string(35, '-');
+    cout << hr << endl;
     cout << msg << endl;
+    cout << hr << endl;
     return 0;
-}
-
-int TestNow(int x) {
-    if(DeBug){cout << x << endl;}
-    if(x==1) {
-    return 1;
-    }else{
-        return x+TestNow(x-1);
-    }
 }
 
 int main()
 {
     int startingNumber = 75;
-    string hr = "------------------------------";
-    cout << hr << endl;
-    cout << "Your starting number is: " << startingNumber << endl << "The ending number is: " << TestNow(startingNumber) << endl;
-    cout << hr << endl;
     return 0;
 }
