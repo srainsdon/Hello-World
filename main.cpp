e4b524017727ebc5604d97615239f42279758db1
Seth Rainsdon <srainsdon>
2015-05-28 21:51:42 -0700

    Not working
    have to figure out how to convert an string to an int

--- main.cpp
+++ main.cpp
@@ -4,7 +4,23 @@
 using namespace std;
 
 bool DeBug = false;
-
+class Message {
+private:
+    int type = 0;
+    string data = "";
+public:
+    Message(string receved){ // this data will be along the lines of the messaging systems wiki
+        std::string tmpType = receved.substr (0,3);
+        type = tmpType; // this needs to be converted to an int
+        data = receved;
+    }
+    int getType(){
+    return type;
+    }
+    string getData(){
+    return data;
+    }
+};
 int sendMsg(string msg) {
     string hr = std::string(35, '-');
     cout << hr << endl;
@@ -13,8 +29,9 @@ int sendMsg(string msg) {
     return 0;
 }
 
-int main()
-{
-    int startingNumber = 75;
+int main(){
+    sendMsg("Server Starting..."); // Not useful
+    Message msg("250Testing message passing"); // test message
+    cout << "Received message:" << endl << "Type: " << msg.getType() << endl << "Data: " << msg.getData() << endl; // message print out
     return 0;
 }
